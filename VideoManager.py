943e3d6c53802724fcba2fb2ef44cabb940d4499
charlotte99969 <charlotte99969@gmail.com>
2023-12-09 00:19:49 +0800

    添加解析度檢查並調整抓取角色資料邏輯

--- VideoManager.py
+++ VideoManager.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from CharacterManager import characters_data
 
 from ImageManager import ImageManager
 from DataManager import DataManager
@@ -21,9 +22,18 @@ class VideoManager:
             DataManager.initialize_output_directory(self.output_directory)
 
             cap = cv2.VideoCapture(self.video_file_path)
-            self.total_progress = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 影片的總幀數
 
+            width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+            if (width != 1920 or height != 1080):
+                print(f"影片解析度：{width} x {height}")
+                self.processing_completed = True
+                return 0
+
+            self.total_progress = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 影片的總幀數
             characters = []
+            counter = 0
 
             while cap.isOpened():
                 ret, frame = cap.read()
@@ -32,11 +42,15 @@ class VideoManager:
                     break
 
                 character_name = ImageManager.extract_character_name(frame)  # 提取腳色名稱的方法需自行實現
-                # print(character_name)
-                if (character_name not in characters):
-                    characters.append(character_name)
-                    frame_filename = os.path.join(self.output_directory, f"{character_name}.jpg")
-                    cv2.imwrite(frame_filename, frame)
+                print(character_name)
+                if(character_name in characters_data.keys()):
+                    if (character_name not in characters or counter < 3):
+                        counter+=1
+                        characters.append(character_name)
+                        frame_filename = os.path.join(self.output_directory, f"{character_name}.jpg")
+                        cv2.imwrite(frame_filename, frame)
+                    else:
+                        counter = 0
 
                 self.current_progress += 1
 
